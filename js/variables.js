a610c83497015c794fd0c652b01d7828ffdd36d5
Matias Goncalvez <matigo31@gmail.com>
2021-11-03 20:04:59 -0300

    Agregando funciones

--- js/variables.js
+++ js/variables.js
@@ -12,43 +12,36 @@ if (nombreUsuario !=""){
 //Comienzo del juego
 alert("Bienvenidos a 'PREGUNTAS Y RESPUESTAS'!")
 alert("Reglas: Tienes 3 intentos por pregunta, obtienes 1 punto por cada respuesta correcta.")
+
+//Puntaje inicial
 puntaje = 0
 
-//1er pregunta
-const actor = "Tom Cruise";
-for (let i = 0; i < 3; i++){
-	let respuesta = prompt("Actor principal de la película Mision Imposible:");
-	if (respuesta == actor){
-		alert("Respuesta correcta!");
-		puntaje++;
-		break;
-	}else {
-		alert("Respuesta incorrecta!");
+//Funciones
+function pregunta(interrogante, solucion) {
+	for (let i = 0; i < 3; i++){
+		let respuesta = prompt(interrogante);
+		if (respuesta == solucion){
+			alert("Respuesta correcta!");
+			puntaje++;
+			break;
+		}else {
+			alert("Respuesta incorrecta!");
+		}
 	}
 }
+
+function mostrar(mensaje) {
+	alert(`Puntaje: ${mensaje}`)
+}
+
+//1er pregunta
+pregunta("¿Quien es el actor principal de la película 'Misión Imposible'?", "Tom Cruise");
+mostrar(puntaje);
 
 //2da pregunta
-const planeta = "Venus"
-for (let i = 0; i < 3; i++){
-	let respuesta = prompt("Planeta mas caliente del sistema solar:");
-	if (respuesta == planeta){
-		alert("Respuesta correcta!");
-		puntaje++;
-		break;
-	}else {
-		alert("Respuesta incorrecta!");
-	}
-}
+pregunta("¿Cual es el planeta mas caliente del Sistema Solar?", "Venus");
+mostrar(puntaje);
 
 //3er pregunta
-const rio = "Amazonas"
-for (let i = 0; i < 3; i++){
-	let respuesta = prompt("Rio mas caudaloso del mundo:");
-	if (respuesta == rio){
-		alert("Respuesta correcta!");
-		puntaje++;
-		break;
-	}else {
-		alert("Respuesta incorrecta!");
-	}
-}
+pregunta("¿Cual es el río mas caudaloso del mundo?", "Amazonas");
+mostrar(puntaje);
